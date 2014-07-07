ae89228f4637c30d27937fad46b818ba817bf08c
dstao3489 <dstao3489@gmail.com>
2014-07-07 00:09:19 -0700

    Renamed demo package to mad package New test directory. Skeleton code for MyLinkedList Sanity tests for MyLinkedList

---
+++ test/mad/MyLinkedListTest.java
@@ -0,0 +1,184 @@
+package mad;
+
+import junit.framework.TestCase;
+
+import java.util.List;
+
+public class MyLinkedListTest extends TestCase {
+
+    private List myList;
+
+    /**
+     * This method will initialize a new MyLinkedList before each test is run
+     */
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        myList = new MyLinkedList();
+    }
+
+    public void testSize() throws Exception {
+        assertEquals(0, myList.size());
+        myList.add("item");
+        assertEquals(1, myList.size());
+    }
+
+    public void testIsEmpty() throws Exception {
+        assertTrue(myList.isEmpty());
+        myList.add("item");
+        assertFalse(myList.isEmpty());
+    }
+
+    public void testContains() throws Exception {
+        String item = "item";
+        assertFalse(myList.contains(item));
+        myList.add(item);
+        assertTrue(myList.contains(item));
+        
+        myList.add(null);
+        assertTrue(myList.contains(null));
+    }
+
+    public void testAdd() throws Exception {
+        assertTrue(myList.add("item"));
+        assertEquals(1, myList.size());
+        assertTrue(myList.add("item2"));
+        assertEquals(2, myList.size());
+        assertTrue(myList.add("item"));
+        assertEquals(3, myList.size());
+        assertTrue(myList.add("another item"));
+        assertEquals(4, myList.size());
+        assertTrue(myList.add(null));
+        assertEquals(5, myList.size());
+    }
+
+    public void testTenThousand() throws Exception {
+        for(int i = 0; i < 10000; i++) {
+            assertTrue(myList.add(Integer.toString(i)));
+        }
+        assertFalse(myList.isEmpty());
+        assertEquals(10000, myList.size());
+        for(int i = 0; i < 10000; i++) {
+            assertTrue(myList.contains(Integer.toString(i)));
+        }
+        for(int i = 0; i < 10000; i++) {
+            assertTrue(myList.remove(Integer.toString(i)));
+        }
+        assertTrue(myList.isEmpty());
+        assertEquals(0, myList.size());
+    }
+
+    public void testRemove() throws Exception {
+        myList.add("item");
+        assertTrue(myList.remove("item"));
+
+        myList.add("item2");
+        assertEquals("item2", myList.remove(0));
+
+        myList.add("a");
+        myList.add("b");
+        assertEquals("b", myList.remove(1));
+        assertTrue(myList.contains("a"));
+    }
+
+    public void testClear() throws Exception {
+        myList.add("a");
+        myList.add("b");
+        myList.clear();
+        assertTrue(myList.isEmpty());
+    }
+
+    public void testGet() throws Exception {
+        myList.add("a");
+        myList.add("b");
+        assertEquals("a", myList.get(0));
+        assertEquals("b", myList.get(1));
+        try {
+            myList.get(2);
+            fail("Should throw IndexOutOfBoundsException");
+        } catch (IndexOutOfBoundsException e) {
+            // pass
+        }
+    }
+
+    public void testSet() throws Exception {
+        myList.add("a");
+        myList.add("b");
+        myList.set(0, "c");
+        myList.set(1, "d");
+        assertEquals("c", myList.get(0));
+        assertEquals("d", myList.get(1));
+        try {
+            myList.set(2, "e");
+            fail("Should throw IndexOutOfBoundsException");
+        } catch (IndexOutOfBoundsException e) {
+            // pass
+        }
+    }
+
+    public void testAddIndex() throws Exception {
+        myList.add("a"); // index 0
+        myList.add("b"); // index 1
+        myList.add("d"); // index 2
+        myList.add(2, "c");
+        assertEquals("a", myList.get(0));
+        assertEquals("b", myList.get(1));
+        assertEquals("c", myList.get(2));
+        assertEquals("d", myList.get(3));
+    }
+
+    public void testRemoveObject() throws Exception {
+        myList.add("a"); // index 0
+        myList.add("b"); // index 1
+        myList.add("c"); // index 2
+        assertEquals(3, myList.size());
+        assertTrue(myList.remove("b"));
+        assertEquals(2, myList.size());
+        assertFalse(myList.contains("b"));
+        assertTrue(myList.remove("c"));
+        assertEquals(1, myList.size());
+        assertFalse(myList.remove("d"));
+        assertEquals(1, myList.size());
+    }
+
+    public void testIndexOf() throws Exception {
+        myList.add("a"); // index 0
+        myList.add("b"); // index 1
+        myList.add("c"); // index 2
+        myList.add("d"); // index 3
+        assertEquals(3, myList.indexOf("d"));
+        assertEquals(2, myList.indexOf("c"));
+        assertEquals(1, myList.indexOf("b"));
+        assertEquals(0, myList.indexOf("a"));
+        assertEquals(-1, myList.indexOf("e"));
+    }
+
+    public void testLastIndexOf() throws Exception {
+        myList.add("a"); // index 0
+        myList.add("b"); // index 1
+        myList.add("c"); // index 2
+        myList.add("d"); // index 3
+        myList.add("a"); // index 4
+        assertEquals(4, myList.lastIndexOf("a"));
+    }
+
+    public void testSubList() throws Exception {
+        myList.add("a"); // index 0
+        myList.add("b"); // index 1
+        myList.add("c"); // index 2
+        myList.add("d"); // index 3
+        myList.add("e"); // index 4
+        List subList = myList.subList(1, 3);
+        assertEquals(5, myList.size());
+        assertEquals(2, subList.size());
+        assertFalse(subList.contains("a"));
+        assertTrue(subList.contains("b"));
+        assertTrue(subList.contains("c"));
+        assertFalse(subList.contains("d"));
+        assertFalse(subList.contains("e"));
+
+        subList = myList.subList(1, 1);
+        assertTrue(subList.isEmpty());
+    }
+
+}
---
+++ test/mad/MySetTest.java
@@ -0,0 +1,79 @@
+package mad;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MySetTest {
+
+	@Test
+	public void testSizeEmpty() {
+		MySet s = new MySet();
+		assertTrue(s.isEmpty());
+	}
+	
+	@Test
+	public void testSizeOne() {
+		MySet s = new MySet();
+		s.add(1);
+		assertEquals(1, s.size());
+	}
+	
+	@Test
+	public void testSize10K() {
+		MySet s = new MySet();
+		for (int i = 0; i < 10000; i++)
+		{
+			s.add(i);
+			assertEquals(i+1, s.size());
+		}
+		assertTrue(s.contains(1));
+		assertTrue(s.contains(9999));
+	}
+	
+	@Test
+	public void testClear() {
+		MySet s = new MySet();
+		s.add(1);
+		s.add(2);
+		assertEquals(2, s.size());
+		s.clear();
+		assertEquals(0, s.size());
+	}
+	
+	@Test
+	public void testAdd() {
+		MySet s = new MySet();
+		s.add(1);
+		assertEquals(1, s.size());
+		assertFalse(s.add(1));
+		assertEquals(1, s.size());
+		assertTrue(s.add(2));
+		assertEquals(2, s.size());
+	}
+	
+	@Test
+	public void testRemove() {
+		MySet s = new MySet();
+		s.add("Macklin");
+		s.add("Luke");
+		assertEquals(2, s.size());
+		assertTrue(s.contains("Macklin"));
+		assertTrue(s.remove("Luke"));
+		assertFalse(s.contains("Luke"));
+	}
+	
+	@Test
+	public void testContainsNull() {
+		MySet s = new MySet();
+		s.add(1);
+		assertFalse(s.contains(null));
+	}
+	
+	@Test
+	public void testAddNull() {
+		MySet s = new MySet();
+		assertFalse(s.add(null));
+	}
+
+}
